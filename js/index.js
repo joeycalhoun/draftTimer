2162a57db5bb024f9c949dbd765f75f0053a37f1
Joey <joeycalhoun13@gmail.com>
2019-08-11 21:47:01 -0500

    start custom round times

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ toggleBtn.addEventListener('click', function(){
     }
  }); */
 
- $('#reset').click(function(){
+$('#reset').click(function () {
     //fillTeams();
     //fillFacts(rawFacts);
     if (watch.isOn || resetBtn.textContent == "Start Draft") {
@@ -42,7 +42,7 @@ toggleBtn.addEventListener('click', function(){
         resetBtn.textContent = 'Resume Draft';
     }
     watch.reset();
- });
+});
 
 // resetBtn.addEventListener('click', function () {
 //     //fillTeams();
@@ -65,15 +65,15 @@ var fillTeams = function () {
 function fillFacts(myFacts) {
     factsArray = new Array(managers.length);
     var pairs = myFacts.split('\n');
-    for(var i = 0; i < pairs.length; i++){
+    for (var i = 0; i < pairs.length; i++) {
         pairs[i] = pairs[i].split(',');
-        for(var j = 0; j < managers.length; j++){
-            if(pairs[i][0].toUpperCase() == managers[j].toUpperCase()){
-                if(factsArray[j] == null){
+        for (var j = 0; j < managers.length; j++) {
+            if (pairs[i][0].toUpperCase() == managers[j].toUpperCase()) {
+                if (factsArray[j] == null) {
                     factsArray[j] = new Array(1);
                     factsArray[j][0] = pairs[i][1];
                 }
-                else{
+                else {
                     var depth = factsArray[j].length;
                     factsArray[j][depth] = pairs[i][1];
                 }
@@ -83,13 +83,13 @@ function fillFacts(myFacts) {
     console.log("Facts Loaded Successfully");
 }
 
-$("#apply").click(function(){
+$("#apply").click(function () {
     //fillTeams();
     readFactsFile();
     //fillFacts(factsFile);
     //console.log(factsArray);
     //console.log(factsArray);
-    setTimeout(function(){
+    setTimeout(function () {
         watch = new StopWatch(
             timeAlotted,
             managers,
@@ -98,14 +98,14 @@ $("#apply").click(function(){
         console.log("Created Watch Successfully")
         configComplete = true;
         //$("#reset").prop('disabled', false);
-    },500);
+    }, 500);
 });
 
-$("#configMenu").click(function(){
-    if(configMenuOpen && configComplete){
+$("#configMenu").click(function () {
+    if (configMenuOpen && configComplete) {
         $("#reset").prop('disabled', false);
     }
-    else{
+    else {
         $("#reset").prop('disabled', true);
     }
     configMenuOpen = !configMenuOpen;
@@ -126,7 +126,7 @@ $("#configMenu").click(function(){
 //}
 
 
-function readFactsFile(){
+function readFactsFile() {
     var fileInput = $("#inputGroupFile04")[0];
     var file = fileInput.files[0];
     var textType = /text.*/;
@@ -161,5 +161,54 @@ $("#applyFacts").click(function () {
         fileDisplayArea.innerText = "File not supported!"
     }
 });
+
+function createRoundTimeTextBoxes(numOfRounds){
+    for(var i = 0; i < numOfRounds; i++){
+
+        var beginColumn = "<div class='col-4'>";
+        var endColumn = "</div>";
+        var beginRow = "<div class='row'>";
+        var endRow = "</div>";
+        var newRow ="<div class='col-12'><div class='input-group mb-3'>"
+        +"<div class='input-group-prepend'>"
+            +"<span class='input-group-text' id='basic-addon1'>&nbspRound "+(i+1)+"&nbsp</span>"
+        +"</div>"
+        +"<input type='text' class='form-control' placeholder='Name' aria-label='Username'"
+            +"aria-describedby='basic-addon1' id='round1'>"
+        +"</div>";
+
+        if(i == numOfRounds-1){
+            console.log("i is "+i+" and I am newRow + end column");
+            $(beginRow+newRow+endRow+endColumn).appendTo('#customRoundTimesWrapper');
+        }
+        else if(i == 0){
+            console.log("i is "+i+" and I am beginColumn + newRow");
+            $(beginColumn+beginRow+newRow+endRow).appendTo('#customRoundTimesWrapper');
+        }
+        else if(i%6 == 0){
+            console.log("i is "+i+" and I am endColumn + newRow");
+            $(endColumn+beginColumn+beginRow+newRow+endRow).appendTo('#customRoundTimesWrapper');
+        }
+        else{
+            console.log("i is "+i+" and I am newRow");
+            $(beginRow+newRow+endRow).appendTo('#customRoundTimesWrapper');
+        }
+        
+    }
+}
+console.log($('input:radio[name="roundTimeOptions"]:checked').val());
+
+$('input:radio[name="roundTimeOptions"]').change(function (){
+
+        if ($(this).val() == 'rbUniformTimes') {
+            console.log("Uniform");
+            $("#txtTimePerPick").prop("disabled", false);
+        }
+        else {
+            console.log("Custom");
+            $("#txtTimePerPick").prop("disabled", true);
+            createRoundTimeTextBoxes($('#txtNumRounds').val());
+        }
+    });
 
 
